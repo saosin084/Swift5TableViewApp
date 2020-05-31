621b90d6c720e0271c09209e0117c2c9253cbe71
Yuta <yuta5196@gmail.com>
2020-05-31 16:59:25 +0900

    Initial Commit

---
+++ Swift5TableViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5TableViewApp
+//
+//  Created by yuta on 2020/05/31.
+//  Copyright © 2020 yuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
